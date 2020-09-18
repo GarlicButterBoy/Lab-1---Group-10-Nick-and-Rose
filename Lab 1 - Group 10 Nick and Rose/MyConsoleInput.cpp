c390716a83359abf4edc60dadf91dc478ca37f0c
Nick Sturch-Flint <nick.rulez@live.com>
2020-09-18 13:12:35 -0400

    Created the three files, added some basic code

---
+++ Lab 1 - Group 10 Nick and Rose/MyConsoleInput.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <limits> //numeric_limits
+#include <cfloat> //limits of DBL and INT Min and Max
+#include "MyConsoleInput.h"
---
+++ Lab 1 - Group 10 Nick and Rose/WorkTicket.cpp
@@ -0,0 +1,35 @@
+/*
+ *Name: Nick Sturch-Flint (100303769) & Rose Nguyen (100XXXXXX)
+ *Program Name: Review of Classes and Objects: Work Tickets
+ *Program Description: An application that stores information about client requests
+ */
+
+#include <iostream> //cin, cout
+#include <iomanip>  //fixed, setprecision()
+#include <sstream>  //string streams
+#include <cmath>    //sqrt()
+#include <limits>   //INT_MAX
+#include <stdexcept>//out_of_range
+
+
+using namespace std;
+
+class WorkTicket
+{
+public:
+	//Constructors: Initialize Objects
+
+	//Accessors: Query the state of the object
+
+	//Mutators: Change the object state
+private:
+	//private data members
+
+}; //end of WorkTicket Class
+
+int main()
+{
+	//Declarations
+
+
+} //end of main
